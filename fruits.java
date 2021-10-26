de0c8d1b3a12c3d3e7664a86853c2dfd5005d38d
ThaiyalMNayagi <92416688+ThaiyalMNayagi@users.noreply.github.com>
2021-10-27 00:34:39 +0530

    Add files via upload

--- fruits.java
+++ fruits.java
@@ -7,17 +7,28 @@ import java.util.stream.*;
      public static void main(String[] args)
      {
          System.out.println("sort based on name");
-         Collections.sort(li,Comparator.comparing((food Name) ->Name.getName()));
-         li.stream().forEach(System.out::println);
+         Collections.sort(li1,Comparator.comparing((food Name) ->Name.getName()));
+         li1.stream().forEach(System.out::println);
          System.out.println("sort based on price");
-         Collections.sort(li,Comparator.comparing((food Price) -> Price.getPrice()));
-         li.stream().forEach(System.out::println);
+         Collections.sort(li1,Comparator.comparing((food Price) -> Price.getPrice()));
+         li1.stream().forEach(price ->System.out.println(price));
      }
+     static List li1=new ArrayList<>();
      private static List li=new ArrayList<>();
      static{
          li.add(new food("Noodles",205,"Chinese"));
          li.add(new food("Idly",175,"Indian"));
          li.add(new food("Enchiladas",200,"Italian"));
+         li1.addAll(li);
+         
+     }
+     private static List li2=new ArrayList<>();
+     static{
+         li2.add(new food("Fried rice",300,"Chinese"));
+         li2.add(new food("Dosa",75,"Indian"));
+         li2.add(new food("Ravioli",150,"Italian"));
+          li1.addAll(li2);
+         System.out.println(li1);
      }
  }
 
