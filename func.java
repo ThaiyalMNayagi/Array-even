8b0e065d251d6b03c97bb7d64dc0c42e8438a4c3
ThaiyalMNayagi <92416688+ThaiyalMNayagi@users.noreply.github.com>
2021-11-10 15:58:45 +0530

    Add files via upload

--- func.java
+++ func.java
@@ -12,13 +12,15 @@ import java.util.*;
         fruits3[] f3=new fruits3[f.length+f2.length];
         int price;
         String name;
+        String color;
         System.out.println("Enter first Array");
         for(int i=0;i<f.length;i++)
         {
             name=s.nextLine();
             price=s.nextInt();
             s.nextLine();
-            f[i]=new fruits(name,price);
+            color=s.nextLine();
+            f[i]=new fruits(name,price,color);
         }
         System.out.println("Enter second Array");
         for(int i=0;i<f2.length;i++)
@@ -26,38 +28,44 @@ import java.util.*;
             name=s.nextLine();
             price=s.nextInt();
             s.nextLine();
-            f2[i]=new fruits2(name,price);
+            color=s.nextLine();
+            f2[i]=new fruits2(name,price,color);
         }
         for(int i=0;i<f.length;i++)
         {
             name=f[i].getName();
              price=f[i].getprice();
-              f3[i]=new fruits3(name,price);
+             color=f[i].getColor();
+              f3[i]=new fruits3(name,price,color);
         }
         int k=0;
         for(int i=f.length;i<f3.length;i++)
         {
             name=f2[k].getName();
             price=f2[k].getprice();
-             f3[i]=new fruits3(name,price);
+            color=f2[k].getColor();
+             f3[i]=new fruits3(name,price,color);
              k++;
         }
 
         
         fruits[] a =sortArr(f);
+        System.out.println("First sorted list");
         for(int i=0;i<a.length;i++)
         {
-            System.out.println(a[i].getprice());
+            System.out.println("The price of "+a[i].getName()+" is "+a[i].getprice()+" whose color is "+a[i].getColor());
         }
         fruits2[] b =sortArr2(f2);
+        System.out.println("Second sorted list");
         for(int i=0;i<b.length;i++)
         {
-            System.out.println(b[i].getprice());
+            System.out.println("The price of "+b[i].getName()+" is "+b[i].getprice()+" whose color is "+b[i].getColor());  
         }
         fruits3[] c =sortArr3(f3);
-        for(int i=0;i<f3.length;i++)
+        System.out.println("Total  sorted list");
+        for(int i=0;i<c.length;i++)
         {
-            System.out.println(f3[i].getprice());
+            System.out.println("The price of "+c[i].getName()+" is "+c[i].getprice()+" whose color is "+c[i].getColor());
         }
         
     }
@@ -119,13 +127,13 @@ class fruits
 
     private int price;
     private String name;
-
-    fruits(String name,int price)
+    private String color;
+    fruits(String name,int price,String color)
     {
         super();
         this.name=name;
         this.price=price;
-
+        this.color=color;
     }
     public String getName()
     {
@@ -142,6 +150,14 @@ class fruits
     public void setPrice(int price)
     {
         this.price=price;
+    }
+    public String getColor()
+    {
+        return color;
+    }
+    public void setColor(String color)
+    {
+        this.color=color;
     }
 } 
 
@@ -150,13 +166,13 @@ class fruits2
 
     private int price;
     private String name;
-
-    fruits2(String name,int price)
+     private String color;
+    fruits2(String name,int price,String color)
     {
         super();
         this.name=name;
         this.price=price;
-
+        this.color=color;
     }
     public String getName()
     {
@@ -173,6 +189,14 @@ class fruits2
     public void setPrice(int price)
     {
         this.price=price;
+    }
+    public String getColor()
+    {
+        return color;
+    }
+    public void setColor(String color)
+    {
+        this.color=color;
     }
 } 
 class fruits3
@@ -180,13 +204,13 @@ class fruits3
 
     private int price;
     private String name;
-
-    fruits3(String name,int price)
+    private String color;
+    fruits3(String name,int price,String color)
     {
         super();
         this.name=name;
         this.price=price;
-
+        this.color=color;
     }
     public String getName()
     {
@@ -203,6 +227,14 @@ class fruits3
     public void setPrice(int price)
     {
         this.price=price;
+    }
+    public String getColor()
+    {
+        return color;
+    }
+    public void setColor(String color)
+    {
+        this.color=color;
     }
 } 
                       
